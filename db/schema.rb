8f893feffcfe757257af138252199221e88d4404
Eric XIA <rxia0914@outlook.com>
2019-01-10 15:04:24 -0500

    edit users adding

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190110170407) do
+ActiveRecord::Schema.define(version: 20190110191815) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "descrition"
-    t.datetime "create_at"
+    t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
   end
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20190110170407) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
